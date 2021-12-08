854759b97f71a3b76916e1098e8833e8a31ec0ef
Wiktor <75067662+wiktorczerwinski@users.noreply.github.com>
2021-12-08 17:12:09 +0100

    Add files via upload

---
+++ spaceship.php
@@ -0,0 +1,16 @@
+<?php
+
+class Spaceship{
+    // Properties
+    public bool $isAlive;
+    public int $fuel;
+    public int $hitPoints;
+    public int $ammo;
+
+    // Constructor
+
+    // Methods | functions
+
+    // Get | set
+
+}
